d771ce9845596c0e23c5dd470eb6cb6cd4c7d0b9
ridnA314 <ridnA776@gmail.com>
2026-09-02 14:35:09 +0900

    feat : Add fire cooltime

--- Assets/02.Scripts/Player/PlayerFire.cs
+++ Assets/02.Scripts/Player/PlayerFire.cs
@@ -5,20 +5,32 @@ public class PlayerFire : MonoBehaviour
     public GameObject BulletPrefab;
     public Transform FirePointL;
     public Transform FirePointR;
+
+    public float FireCoolTime;
+    
+    private float _timer;
+
+    private void Awake()
+    {
+        _timer = FireCoolTime;
+    }
     
     private void Update()
     {
+        _timer += Time.deltaTime;
         Fire();
     }
 
     private void Fire()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && _timer > FireCoolTime)
         {
             GameObject bulletL = Instantiate(BulletPrefab);
             bulletL.transform.position = FirePointL.position;
             GameObject bulletR = Instantiate(BulletPrefab);
             bulletR.transform.position = FirePointR.position;
+            
+            _timer = 0f;
         }
     }
 }
