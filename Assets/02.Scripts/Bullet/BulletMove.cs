bd26bf509f57f654c8dbe11428f2e7c53041de11
ridnA314 <ridnA776@gmail.com>
2026-09-02 14:01:58 +0900

    feat : Add Bullet and Bullet move

---
+++ Assets/02.Scripts/Bullet/BulletMove.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class BulletMove : MonoBehaviour
+{
+    public float SpeedScalar;
+    
+    private void Update()
+    {
+        Move();
+    }
+    
+    private void Move()
+    {
+        Vector2 direction = Vector2.up;
+        Vector2 distance = direction * SpeedScalar * Time.deltaTime;
+        
+        transform.Translate(distance);
+    }
+}
