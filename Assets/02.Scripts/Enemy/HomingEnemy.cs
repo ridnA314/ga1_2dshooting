9725efedab41addb184576c2e63b40f5078f25ef
ridnA314 <ridnA776@gmail.com>
2026-09-03 15:01:31 +0900

    chore: player 참조 위치 변경

--- Assets/02.Scripts/Enemy/HomingEnemy.cs
+++ Assets/02.Scripts/Enemy/HomingEnemy.cs
@@ -2,9 +2,16 @@ using UnityEngine;
 
 public class HomingEnemy : Enemy
 {
+    private GameObject _player;
+
+    private void Start()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player");
+    }
+
     public override void Move()
     {
-        Vector2 targetDirection = GameObject.FindWithTag("Player").transform.position - transform.position;
+        Vector2 targetDirection = _player.transform.position - transform.position;
         targetDirection = targetDirection.normalized;
         Vector2 distance = targetDirection * _moveSpeedScalar * Time.deltaTime;
         transform.Translate(distance);
