8e771167c7bd21dc3ad78fcb2dbe3e2ba99d36ae
ridnA314 <ridnA776@gmail.com>
2026-09-03 12:52:00 +0900

    feat: add collider and rigidyBody at Player, Enemy, Bullet

--- Assets/02.Scripts/Bullet/Bullet.cs
+++ Assets/02.Scripts/Bullet/Bullet.cs
@@ -1,8 +1,9 @@
+using System;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public float SpeedScalar;
+    public float MoveSpeedScalar;
 
     private void Update()
     {
@@ -12,8 +13,23 @@ public class Bullet : MonoBehaviour
     private void Move()
     {
         Vector2 direction = Vector2.up;
-        Vector2 distance = direction * SpeedScalar * Time.deltaTime;
+        Vector2 distance = direction * MoveSpeedScalar * Time.deltaTime;
 
         transform.Translate(distance);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("충돌!");
+    }
+
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        Debug.Log("충돌중");
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        Debug.Log("충돌 끝!");
     }
 }
