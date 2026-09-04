5b83ab0ebb60781766cc0a5643333c6014ea68c5
ridnA314 <ridnA776@gmail.com>
2026-09-04 16:51:28 +0900

    feat: add drop item at enemy by random 3 type

--- Assets/02.Scripts/Enemy/Enemy.cs
+++ Assets/02.Scripts/Enemy/Enemy.cs
@@ -11,6 +11,18 @@ public abstract class Enemy : MonoBehaviour
     [SerializeField]
     private float _power = 10f;
 
+    [Header("Power Item")]
+    [SerializeField]
+    private Item _powerItemPrefab;
+
+    [Header("Health Item")]
+    [SerializeField]
+    private Item _healthItemPrefab;
+
+    [Header("Attack Speed Item")]
+    [SerializeField]
+    private Item _attackSpeedItemPrefab;
+
     protected Transform _playerTransform;
 
     private void Update()
@@ -40,7 +52,33 @@ public abstract class Enemy : MonoBehaviour
                 player.TakeDamage(_power);
             }
 
+            DropItem();
             Destroy(gameObject);
         }
+    }
+
+    private void DropItem()
+    {
+        if (_playerTransform == null) return;
+
+        int probability = UnityEngine.Random.Range(0, 100);
+        if (probability > 30) return;
+
+        Item item;
+        if (probability <= 10)
+        {
+            item = _powerItemPrefab;
+        }
+        else if (probability <= 20)
+        {
+            item = _healthItemPrefab;
+        }
+        else
+        {
+            item = _attackSpeedItemPrefab;
+        }
+
+        item = Instantiate(item);
+        item.transform.position = transform.position;
     }
 }
