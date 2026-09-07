ea02084a718edfa1799ef46e2fa2186f86f1e5ee
ridnA314 <ridnA776@gmail.com>
2026-09-07 10:34:16 +0900

    feat: add max health at player

--- Assets/02.Scripts/Player/Player.cs
+++ Assets/02.Scripts/Player/Player.cs
@@ -3,7 +3,15 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField]
-    private float _health = 100f;
+    private float _health;
+
+    [SerializeField]
+    private float _maxHealth = 100f;
+
+    private void Start()
+    {
+        _health = _maxHealth;
+    }
 
     public void TakeDamage(float amount)
     {
@@ -17,5 +25,9 @@ public class Player : MonoBehaviour
     public void GrowUpHealth(float amount)
     {
         _health += amount;
+        if (_health > _maxHealth)
+        {
+            _health = _maxHealth;
+        }
     }
 }
