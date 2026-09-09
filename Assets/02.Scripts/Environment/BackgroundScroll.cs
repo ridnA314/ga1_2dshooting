1230b05c24cc93bca947fef51cb36e5b0aaedc61
ridnA314 <ridnA776@gmail.com>
2026-09-09 13:47:37 +0900

    feat: implementation infinity background by using material offset

---
+++ Assets/02.Scripts/Environment/BackgroundScroll.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class BackgroundScroll : MonoBehaviour
+{
+    private Material _material;
+    private float _offSetY = 0f;
+
+    [SerializeField]
+    private float _scrollSpeed = 0.1f;
+
+    private void Awake()
+    {
+        _material = GetComponent<Renderer>().material;
+    }
+
+    private void Update()
+    {
+        _offSetY += Time.deltaTime * _scrollSpeed;
+        _material.mainTextureOffset = new Vector2(0, _offSetY);
+    }
+}
