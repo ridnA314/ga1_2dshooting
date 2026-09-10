d53887c69317204eff962755b5472c3149eb3d07
ridnA314 <ridnA776@gmail.com>
2026-09-10 10:51:43 +0900

    refactor: chaching target at auto move and  extraction method

--- Assets/02.Scripts/Player/PlayerAutoMove.cs
+++ Assets/02.Scripts/Player/PlayerAutoMove.cs
@@ -7,34 +7,55 @@ public class PlayerAutoMove : MonoBehaviour
     [SerializeField]
     private float _speedScalar;
 
+    private GameObject _target = null;
+
     private void Update()
+    {
+        if (_target == null)
+        {
+            FindNearestTarget();
+        }
+
+        Move();
+    }
+
+    private void Move()
+    {
+        if (_target == null) return;
+
+        //2. get direction
+        Vector3 direction = _target.transform.position - transform.position;
+        direction.Normalize();
+        direction.y = 0;
+
+        //3.move by speed
+        transform.position += direction * _speedScalar * Time.deltaTime;
+    }
+
+    private void FindNearestTarget()
     {
         //1.get Target
         GameObject[] targets = GameObject.FindGameObjectsWithTag("Enemy");
-        if (targets.Length == 0) return;
+        if (targets.Length == 0)
+        {
+            _target = null;
+            return;
+        }
 
-        GameObject target = targets[0];
+        _target = targets[0];
         float minDistance = float.MaxValue;
         float distance;
         //1-1. find nearest target
         foreach (GameObject enemy in targets)
         {
             // calculate distance
-            distance = Vector2.Distance(transform.position, target.transform.position);
+            distance = Vector2.Distance(transform.position, _target.transform.position);
             if (minDistance > distance)
             {
                 // change target
                 minDistance = distance;
-                target = enemy;
+                _target = enemy;
             }
         }
-
-        //2. get direction
-        Vector3 direction = target.transform.position - transform.position;
-        direction.Normalize();
-        direction.y = 0;
-
-        //3.move by speed
-        transform.position += direction * _speedScalar * Time.deltaTime;
     }
 }
